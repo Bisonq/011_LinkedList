d65f2c39d4e2a7fbeefa7d2af62ca9cac3da4f2a
Tomasz Sosnowski <t.sosnowski2508@wp.pl>
2020-07-11 20:51:12 +0200

    added List interface with descriptions

---
+++ src/main/java/list/List.java
@@ -0,0 +1,87 @@
+package list;
+
+public interface List<E extends Comparable<E>> {
+
+    /**
+     * Add element to top of list
+     * @param element element to be added, cannot be null
+     * if element is null then IllegalArgumentException will be throw
+     */
+    void addFirst(E element);
+
+    /**
+     * Add element to the end of list
+     * @param element element to be added, cannot be null
+     * if element is null then IllegalArgumentException will be throw
+     */
+    void addLast(E element);
+
+    /**
+     * Remove element to top of list
+     * @return true - if element has been removed
+     * otherwise false, if element has not been removed
+     * for example: when List is empty
+     */
+    boolean removeFirst();
+
+    /**
+     * Remove element to the end of list
+     * @return true - if element has been removed
+     * otherwise false, if element has not been removed
+     * for example: when List is empty
+     */
+    boolean removeLast();
+
+    /**
+     * Remove first element from the list that is equals to given
+     * @param element, element by which we will compare
+     * @return true - if element has been removed
+     * otherwise false, if element has not been removed
+     * for example: when List is empty or the given element is not in the list
+     */
+    boolean remove(E element);
+
+    /**
+     * Return List size
+     * @return number of elements in the list
+     */
+    int size();
+
+    /**
+     * Return minimal element in the list using Comparable tool
+     * @return minimal element in the list
+     */
+    E getMin();
+
+    /**
+     * Return maximum element in the list using Comparable tool
+     * @return maximal element in the list
+     */
+    E getMax();
+
+    /**
+     * Check if the list is empty
+     * @return true, if the list is empty
+     * otherwise false, when the list is not empty
+     */
+    boolean isEmpty();
+
+    /**
+     * Check if the list contain element
+     * @param element value sought
+     * @return true, if element is in the list
+     * otherwise false, if element is not in the list
+     */
+    boolean contains(E element);
+
+    /**
+     * Clear the list
+     */
+    void clear();
+
+    /**
+     * Convert list into element type Array
+     * @return element type Array
+     */
+    E[] toArray();
+}
